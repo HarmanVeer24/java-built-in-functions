1feed6b95cda106014c265fc0e6b193c5c69aa4f
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-07 23:41:49 +0530

    [Harman] ADD:program to print the time according to diff time zones

---
+++ TimeZone.java
@@ -0,0 +1,16 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+//getting two diff time zone 
+public class TimeZones {
+    public static void main(String[] args) {
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        System.out.println("Current time in GMT: " + gmtTime);
+        System.out.println("Current time in IST: " + istTime);
+        System.out.println("Current time in PST: " + pstTime);
+    }
+}
+//output will change according to time
+
